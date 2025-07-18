be24079b890b5dd8092b725cd51e13d93bd8fd20
angelbt12 <angeldavidbaracaldo056@gmail.com>
2025-07-18 09:22:10 -0500

    fix: route setting

--- src/routes/certifications.tsx
+++ src/routes/certifications.tsx
@@ -2,12 +2,41 @@ import { Route, Routes } from "react-router-dom";
 
 import { CertificationsOptions } from "@pages/certifications";
 import { NewCertification } from "@pages/certifications/NewCertification";
+import { useAppContext } from "@context/AppContext";
+
+function PrivilegedRoute({
+  children,
+  requiredPrivilege,
+}: {
+  children: React.ReactNode;
+  requiredPrivilege: string;
+}) {
+  const { staffUseCasesData } = useAppContext();
+
+  const hasPrivilege =
+    Array.isArray(staffUseCasesData?.listOfUseCases) &&
+    staffUseCasesData.listOfUseCases.includes(requiredPrivilege);
+
+  if (!hasPrivilege) {
+    return <></>;
+  }
+
+  return <>{children}</>;
+}
 
 function CertificationsRoutes() {
   return (
     <Routes>
       <Route path="/" element={<CertificationsOptions />} />
-      <Route path="/new-certification" element={<NewCertification />} />
+
+      <Route
+        path="/new-certification"
+        element={
+          <PrivilegedRoute requiredPrivilege="RequestCertificate">
+            <NewCertification />
+          </PrivilegedRoute>
+        }
+      />
     </Routes>
   );
 }
