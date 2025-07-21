c213b021d44b354e11b98c2782262085f052280f
angelbt12 <angeldavidbaracaldo056@gmail.com>
2025-07-21 11:52:20 -0500

    fix: privilege route adjustment

--- src/routes/certifications.tsx
+++ src/routes/certifications.tsx
@@ -2,27 +2,7 @@ import { Route, Routes } from "react-router-dom";
 
 import { CertificationsOptions } from "@pages/certifications";
 import { NewCertification } from "@pages/certifications/NewCertification";
-import { useAppContext } from "@context/AppContext";
-
-function PrivilegedRoute({
-  children,
-  requiredPrivilege,
-}: {
-  children: React.ReactNode;
-  requiredPrivilege: string;
-}) {
-  const { staffUseCasesData } = useAppContext();
-
-  const hasPrivilege =
-    Array.isArray(staffUseCasesData?.listOfUseCases) &&
-    staffUseCasesData.listOfUseCases.includes(requiredPrivilege);
-
-  if (!hasPrivilege) {
-    return <></>;
-  }
-
-  return <>{children}</>;
-}
+import { PrivilegedRoute } from "./privilegedRoute";
 
 function CertificationsRoutes() {
   return (
