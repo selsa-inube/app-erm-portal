276ad24c7fcbfd2757fc4f60098ca315936f94c3
angelbt12 <angeldavidbaracaldo056@gmail.com>
2026-01-05 16:35:32 -0500

    feat: iauth update

--- src/pages/logOut/index.tsx
+++ src/pages/logOut/index.tsx
@@ -1,11 +1,13 @@
 import { useEffect } from "react";
 
-import { useIAuth } from "@inube/iauth-react";
+import { useSignOut } from "@hooks/useSignOut";
 
 export function LogOut() {
-  const { logout } = useIAuth();
+  const { signOut } = useSignOut();
+
   useEffect(() => {
-    logout();
-  }, [logout]);
+    signOut("/");
+  }, [signOut]);
+
   return null;
 }
